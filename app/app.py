fcaa3b502fefc668233b6fde83c5be7d03bc26ef
sap33 <sap33@njit.edu>
2020-12-04 11:44:20 -0500

    PUSH : changes

--- app/app.py
+++ app/app.py
@@ -9,10 +9,10 @@ from dateutil.parser import parse
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
 
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_PORT'] = 32000
+app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'covidInsight'
 mysql.init_app(app)
 
